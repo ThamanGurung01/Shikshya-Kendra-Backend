a5647b26f8ca677085eab9638f0ad9134d7f0138
Thaman Gurung <tgurung519@gmail.com>
2026-04-26 08:29:36 +0545

    update: env update

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 import cors from "cors";
 const app = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL??'http://localhost:5173',
+  origin: process.env.FRONTEND_URL??'http://localhost:3000',
   credentials: true,
 }))
 app.use(express.json());
